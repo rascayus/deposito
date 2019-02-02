3e8373fdab3d4f98831da36f57d80732ffd08476
Juan Diego Alonso <tontolanona@gmail.com>
2019-02-02 22:31:55 +0100

    Añadida Suite de pruebas

---
+++ DepositoCombustible/src/Suite_de_Pruebas.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestConsumo.class, TestLlenado.class, TestMitad.class, TestNivelActual.class, TestVacio.class,
+		TestValorLimite.class })
+public class Suite_de_Pruebas {
+
+}
